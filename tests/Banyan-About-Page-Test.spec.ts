33ead3f6541ef9052a0394c60c39a6d2189dd789
gr3y6h0st <jayvy19@gmail.com>
2023-04-05 00:10:01 -0700

    Added new About Page Test
    Test Case example for Company Mission Statement inside About Page section of Banyan Website.

---
+++ tests/Banyan-About-Page-Test.spec.ts
@@ -0,0 +1,13 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  //navigate to Banyan Infrastructure website.
+  await page.goto('https://www.banyaninfrastructure.com/');
+  //click the About section link.
+  await page.getByRole('navigation').getByRole('link', { name: 'About' }).click();
+  //Check to make sure the new page's URL contains the text: 'about'
+  await expect(page).toHaveURL(/.*about/);
+  //check for a header role that contains the Company Mission.
+  const element = page.getByText('Our mission is to accelerate the deployment of sustainable infrastructure projects everywhere.');
+  expect(element !== undefined).toBeTruthy();
+});
