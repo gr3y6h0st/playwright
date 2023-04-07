1145018a51bf6f0044ec4c8882a4e747dab6ba8c
gr3y6h0st <jayvy19@gmail.com>
2023-04-07 10:32:38 -0700

    Combined Join Us and About Page Test Case. Changed Base URL in playwright config file to Bayan Homepage. Removed duplicate test files.
    About Page and Join Us Test Cases combined into a single file. Utilized playwright config file to navigate to Banyan Homepage instead of hardcoding the URL into multiple test cases. Deleted unnecessary/duplicate files.

--- tests/Banyan-About-Page-Test.spec.ts
+++ tests/Banyan-About-Page-Test.spec.ts
@@ -1,13 +1,28 @@
 import { test, expect } from '@playwright/test';
 
-test('test', async ({ page }) => {
+test('first', async ({ page }) => {
   //navigate to Banyan Infrastructure website.
-  await page.goto('https://www.banyaninfrastructure.com/');
+  await page.goto('/settings');
+
   //click the About section link.
   await page.getByRole('navigation').getByRole('link', { name: 'About' }).click();
+
   //Check to make sure the new page's URL contains the text: 'about'
   await expect(page).toHaveURL(/.*about/);
+
   //check for a header role that contains the Company Mission.
   let companyMission = "Our mission is to accelerate the deployment of sustainable infrastructure projects everywhere.";
+  //use locator to find header w/ Company Mission Statement.
   const locator = page.getByRole('heading', { name: 'Our mission is to accelerate the deployment of sustainable infrastructure projects everywhere.' });
+  //webFirst Assert company mission text on webpage vs companyMission value.
   await expect(locator).toHaveText(companyMission)});
+
+test('second', async ({ page }) => {
+  //navigate to Bayan Infrastructure home page.
+  await page.goto('/settings');
+
+  //click on Join Us link
+  await page.getByRole('link', { name: 'Join Us' }).click();
+  //test expect URL to have 'greenhouse'
+  await expect(page).toHaveURL(/.*greenhouse/);
+});
--- tests/Banyan-Join-Us-URL-Fail.spec.ts
+++ tests/Banyan-Join-Us-URL-Fail.spec.ts
@@ -2,10 +2,10 @@ import { test, expect } from '@playwright/test';
 
 test('test', async ({ page }) => {
     //navigate to Bayan Infrastructure home page.
-  await page.goto('https://www.banyaninfrastructure.com/');
+  await page.goto('/settings');
 
   //click on Join Us link
   await page.getByRole('link', { name: 'Join Us' }).click();
-  //test expect URL to have 'greenhouse'
+  //test for expected URL to contain 'greenhousetea' but got (intentionally fail).
   await expect(page).toHaveURL(/.*greenhousetea/);
 });
